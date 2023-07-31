b7f5b9860638c98063a88ffe6a89e55556560c3f
Laurent Doguin <laurent.doguin@gmail.com>
2023-07-31 12:24:18 +0200

    use proper API URL

--- testConnection.sh
+++ testConnection.sh
@@ -1,8 +1,6 @@
 until [ \
-  "$(curl -s -w '%{http_code}' -o /dev/null "http://localhost:8080/api/articles")" \
+  "$(curl -s -w '%{http_code}' -o /dev/null $1)" \
   -eq 200 ]
 do
   sleep 5
 done
-
-
